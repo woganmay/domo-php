1d979370454069ac93bc729a93b947160270f237
Wogan May <wogan.may@wundermanthompson.com>
2023-07-08 15:40:09 +0200

    Implementing Activity Log API

--- tests/ActivityLogServiceTest.php
+++ tests/ActivityLogServiceTest.php
@@ -5,8 +5,20 @@ use WoganMay\DomoPHP\Client;
 
 final class ActivityLogServiceTest extends TestCase
 {
-    public function test()
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testGetAuditLogs()
     {
         $client = new Client();
+
+        $logs = $client->activityLog()->get(0);
+
+        $this->assertIsArray($logs);
+
+        // This assertion assumes that there is always at least 1 audit log event in your Domo instance,
+        // which should be mostly true!
+        $this->assertGreaterThan(0, count($logs));
+
     }
 }
